4df6026c5a19733b1bf684015576e7f0ecc23f6a
Vaisakh Murali <mvaisakh@statixos.com>
2026-06-01 16:15:34 +0530

    recovery: initial high contrast UI implementation
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

---
+++ cerium_ui.cpp
@@ -0,0 +1,93 @@
+#include "cerium_ui.h"
+#include "minui/minui.h"
+#include <android-base/logging.h>
+#include <string>
+#include <vector>
+
+CeriumRecoveryUI::CeriumRecoveryUI() : 
+    screen_width(0), screen_height(0), 
+    block_padding(24), block_margin(12),
+    char_width(0), char_height(0) {}
+
+void CeriumRecoveryUI::Init() {
+    ScreenRecoveryUI::Init();
+    
+    screen_width = gr_fb_width();
+    screen_height = gr_fb_height();
+    
+    gr_font_size(gr_sys_font(), &char_width, &char_height);
+    
+    if (screen_width > 1080) {
+        block_padding = 48;
+        block_margin = 20;
+    }
+}
+
+void CeriumRecoveryUI::DrawBackground() {
+    gr_color(15, 23, 42, 255); 
+    gr_fill(0, 0, screen_width, screen_height);
+}
+
+void CeriumRecoveryUI::DrawCeriumHeader() {
+    int y_cursor = 60;
+
+    gr_color(0, 0, 0, 255);
+    gr_fill(0, 0, screen_width, y_cursor + char_height + 40);
+    gr_color(14, 165, 233, 255); 
+    gr_fill(0, y_cursor + char_height + 38, screen_width, y_cursor + char_height + 42);
+    gr_color(248, 250, 252, 255);
+    std::string title = "CERIUM RECOVERY";
+    int x_pos = (screen_width - (title.length() * char_width)) / 2;
+    gr_text(gr_sys_font(), x_pos, y_cursor + char_height, title.c_str(), 1);
+}
+
+void CeriumRecoveryUI::DrawCeriumMenu() {
+    if (!menu_ || menu_->IsEmpty()) return;
+
+    int menu_start_y = 200;
+    int total_items = menu_->ItemsCount();
+    int current_selection = menu_->selection();
+
+    int block_w = screen_width - (block_margin * 2);
+    int block_h = char_height + (block_padding * 2);
+
+    for (int i = 0; i < total_items; ++i) {
+        int item_top = menu_start_y + (i * (block_h + block_margin));
+        int item_bottom = item_top + block_h;
+
+        if (item_bottom > screen_height - 100) 
+            break;
+
+        std::string label = menu_->ItemAt(i);
+
+        if (i == current_selection) {
+            gr_color(14, 165, 233, 255);
+            gr_fill(block_margin, item_top, block_margin + block_w, item_bottom);
+            gr_color(2, 132, 199, 255);
+            gr_fill(block_margin + 4, item_top + 4, block_margin + block_w - 4, item_top + 6);
+            gr_fill(block_margin + 4, item_bottom - 6, block_margin + block_w - 4, item_bottom - 4);
+            gr_color(15, 23, 42, 255);
+        } else {
+            gr_color(0, 0, 0, 255);
+            gr_fill(block_margin, item_top, block_margin + block_w, item_bottom);
+            gr_color(51, 65, 85, 255);
+            gr_fill(block_margin, item_top, block_margin + block_w, item_top + 2);
+            gr_fill(block_margin, item_bottom - 2, block_margin + block_w, item_bottom);
+            gr_color(226, 232, 240, 255);
+        }
+
+        int text_x = block_margin + block_padding;
+        int text_y = item_top + block_padding + char_height;
+
+        gr_text(gr_sys_font(), text_x, text_y, label.c_str(), (i == current_selection) ? 1 : 0);
+    }
+}
+
+void CeriumRecoveryUI::DrawMenu() {
+    DrawCeriumHeader();
+    DrawCeriumMenu();
+}
+
+Device* make_device() {
+    return new CeriumDevice(new CeriumRecoveryUI);
+}
---
+++ cerium_ui.h
@@ -0,0 +1,32 @@
+#ifndef __CERIUM_RECOVERY_UI_H__
+#define __CERIUM_RECOVERY_UI_H__
+
+#include "recovery_ui/device.h"
+#include "recovery_ui/screen_ui.h"
+
+class CeriumRecoveryUI : public ScreenRecoveryUI {
+public:
+    CeriumRecoveryUI();
+    
+    void Init() override;
+    void DrawBackground() override;
+    void DrawMenu() override;
+
+protected:
+    int screen_width;
+    int screen_height;
+    int block_padding;
+    int block_margin;
+    int char_width;
+    int char_height;
+
+    void DrawCeriumHeader();
+    void DrawCeriumMenu();
+};
+
+class CeriumDevice : public Device {
+public:
+    explicit CeriumDevice(ScreenRecoveryUI* ui) : Device(ui) {}
+};
+
+#endif // __CERIUM_RECOVERY_UI_H__
