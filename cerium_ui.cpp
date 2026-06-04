1b24648abeb8249bec42bcd7dadc25c27489cc2c
Vaisakh Murali <mvaisakh@statixos.com>
2026-06-04 23:53:14 +0530

    recovery: Revamp ui
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>

--- cerium_ui.cpp
+++ cerium_ui.cpp
@@ -1,30 +1,50 @@
 // Copyright (C) 2026 Project Cerium
 
 #include "cerium_ui.h"
+#include "recovery_ui/menu.h"
 #include "minui/minui.h"
 #include <android-base/logging.h>
-#include <string>
-#include <vector>
 
-CeriumRecoveryUI::CeriumRecoveryUI() :
+const std::vector<std::string>& CeriumDevice::GetMenuItems() {
+    if (filtered_menu_.empty()) {
+        std::vector<std::string> default_items = Device::GetMenuItems();
+        for (const auto& item : default_items) {
+            if (item.find("graphics test") != std::string::npos ||
+                item.find("locale test") != std::string::npos ||
+                item.find("rescue") != std::string::npos) {
+                continue;
+            }
+            filtered_menu_.push_back(item);
+        }
+    }
+    return filtered_menu_;
+}
+
+CeriumRecoveryUI::CeriumRecoveryUI() : 
     screen_width(0), screen_height(0), 
-    block_padding(24), block_margin(12),
+    block_padding(32), block_margin(16),
     char_width(0), char_height(0) {}
 
 bool CeriumRecoveryUI::Init(const std::string& locale) {
     bool result = ScreenRecoveryUI::Init(locale);
-
     screen_width = gr_fb_width();
     screen_height = gr_fb_height();
-    
     gr_font_size(gr_sys_font(), &char_width, &char_height);
     
     if (screen_width > 1080) {
-        block_padding = 48;
-        block_margin = 20;
+        block_padding = 64;
+        block_margin = 32;
     }
-
     return result;
+}
+
+void CeriumRecoveryUI::update_screen_locked() {
+    gr_color(0, 0, 0, 255);
+    gr_clear();
+
+    draw_background_locked();
+    draw_foreground_locked();
+    gr_flip();
 }
 
 void CeriumRecoveryUI::draw_background_locked() {
@@ -33,13 +53,13 @@ void CeriumRecoveryUI::draw_background_locked() {
 }
 
 void CeriumRecoveryUI::DrawCeriumHeader() {
-    int y_cursor = 60; 
+    int y_cursor = 150; 
 
     gr_color(0, 0, 0, 255);
-    gr_fill(0, 0, screen_width, y_cursor + char_height + 40);
+    gr_fill(0, 0, screen_width, y_cursor + char_height + 60);
 
     gr_color(14, 165, 233, 255); 
-    gr_fill(0, y_cursor + char_height + 38, screen_width, y_cursor + char_height + 42);
+    gr_fill(0, y_cursor + char_height + 56, screen_width, y_cursor + char_height + 60);
 
     gr_color(248, 250, 252, 255); 
     std::string title = "CERIUM RECOVERY";
@@ -51,10 +71,9 @@ void CeriumRecoveryUI::DrawCeriumMenu() {
     if (!menu_) return;
 
     TextMenu* text_menu = static_cast<TextMenu*>(menu_.get());
-
     if (!text_menu || text_menu->MenuStart() == text_menu->MenuEnd()) return;
 
-    int menu_start_y = 200; 
+    int menu_start_y = 300; 
     size_t current_selection = text_menu->selection();
 
     int block_w = screen_width - (block_margin * 2);
@@ -78,8 +97,8 @@ void CeriumRecoveryUI::DrawCeriumMenu() {
             gr_fill(block_margin, item_top, block_margin + block_w, item_bottom);
 
             gr_color(2, 132, 199, 255);
-            gr_fill(block_margin + 4, item_top + 4, block_margin + block_w - 4, item_top + 6);
-            gr_fill(block_margin + 4, item_bottom - 6, block_margin + block_w - 4, item_bottom - 4);
+            gr_fill(block_margin + 6, item_top + 6, block_margin + block_w - 6, item_top + 8);
+            gr_fill(block_margin + 6, item_bottom - 8, block_margin + block_w - 6, item_bottom - 6);
 
             gr_color(15, 23, 42, 255);
         } else {
@@ -87,14 +106,14 @@ void CeriumRecoveryUI::DrawCeriumMenu() {
             gr_fill(block_margin, item_top, block_margin + block_w, item_bottom);
 
             gr_color(51, 65, 85, 255);
-            gr_fill(block_margin, item_top, block_margin + block_w, item_top + 2);
-            gr_fill(block_margin, item_bottom - 2, block_margin + block_w, item_bottom);
+            gr_fill(block_margin, item_top, block_margin + block_w, item_top + 3);
+            gr_fill(block_margin, item_bottom - 3, block_margin + block_w, item_bottom);
 
             gr_color(226, 232, 240, 255);
         }
 
         int text_x = block_margin + block_padding;
-        int text_y = item_top + block_padding + char_height;
+        int text_y = item_top + (block_h / 2) + (char_height / 2);
 
         gr_text(gr_sys_font(), text_x, text_y, label.c_str(), (i == current_selection) ? 1 : 0);
     }
@@ -105,6 +124,8 @@ void CeriumRecoveryUI::draw_foreground_locked() {
     DrawCeriumMenu();
 }
 
-Device* make_device() {
-    return new CeriumDevice(new CeriumRecoveryUI);
+extern "C" {
+    Device* make_device() {
+        return new CeriumDevice(new CeriumRecoveryUI);
+    }
 }
--- cerium_ui.h
+++ cerium_ui.h
@@ -5,14 +5,16 @@
 
 #include "recovery_ui/device.h"
 #include "recovery_ui/screen_ui.h"
+#include <vector>
+#include <string>
 
 class CeriumRecoveryUI : public ScreenRecoveryUI {
 public:
     CeriumRecoveryUI();
-    
     bool Init(const std::string& locale) override;
 
 protected:
+    void update_screen_locked() override;
     void draw_background_locked() override;
     void draw_foreground_locked() override;
 
@@ -30,6 +32,12 @@ protected:
 class CeriumDevice : public Device {
 public:
     explicit CeriumDevice(ScreenRecoveryUI* ui) : Device(ui) {}
+    
+    // Override the menu list to remove unwanted AOSP items
+    const std::vector<std::string>& GetMenuItems() override;
+
+private:
+    std::vector<std::string> filtered_menu_;
 };
 
 #endif // __CERIUM_RECOVERY_UI_H__
